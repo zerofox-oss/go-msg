7e503a68a65163795b448ec9065b74ffa6092793
Shezad Khan <skhan@zerofox.com>
2023-02-07 13:36:21 +0000

    Add OpenTelemetry support
    Adds a new subpackage in decorators that is a drop in replacement for
    the current opencensus tracing decorators. The new Otel tracing
    decorators is backwards compatible with the previous decorators, but
    will set new spans/traces in the otel format.

---
+++ decorators/otel/tracing/receiver.go
@@ -0,0 +1,123 @@
+package tracing
+
+import (
+	"context"
+	"encoding/base64"
+
+	"github.com/zerofox-oss/go-msg"
+	"go.opencensus.io/trace/propagation"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+
+	ocbridge "go.opentelemetry.io/otel/bridge/opencensus"
+)
+
+var tracer = otel.Tracer("github.com/zerofox-oss/go-msg/decorators/otel")
+
+const traceContextKey = "Tracecontext"
+const traceStateKey = "Tracestate"
+
+type Options struct {
+	SpanName     string
+	StartOptions trace.SpanStartOption
+	OnlyOtel     bool
+}
+
+type Option func(*Options)
+
+func WithSpanName(spanName string) Option {
+	return func(o *Options) {
+		o.SpanName = spanName
+	}
+}
+
+func WithStartOption(so trace.SpanStartOption) Option {
+	return func(o *Options) {
+		o.StartOptions = so
+	}
+}
+
+func WithOnlyOtel(onlyOtel bool) Option {
+	return func(o *Options) {
+		o.OnlyOtel = onlyOtel
+	}
+}
+
+// Receiver Wraps another msg.Receiver, populating
+// the context with any upstream tracing information.
+func Receiver(next msg.Receiver, opts ...Option) msg.Receiver {
+
+	options := &Options{
+		SpanName: "msg.Receiver",
+	}
+
+	for _, opt := range opts {
+		opt(options)
+	}
+
+	return msg.ReceiverFunc(func(ctx context.Context, m *msg.Message) error {
+		ctx, span := withContext(ctx, m, options)
+		defer span.End()
+		return next.Receive(ctx, m)
+	})
+}
+
+// withContext checks to see if a traceContext is
+// present in the message attributes. If one is present
+// a new span is created with that tracecontext as the parent
+// otherwise a new span is created without a parent. A new context
+// which contains the created span well as the span itself
+// is returned
+func withContext(ctx context.Context, m *msg.Message, options *Options) (context.Context, trace.Span) {
+
+	textCarrier := msgAttributesTextCarrier{attributes: &m.Attributes}
+	tmprop := otel.GetTextMapPropagator()
+
+	// if any of the fields used by
+	// the text map propagation is set
+	// we use otel to decode
+	for _, field := range tmprop.Fields() {
+		if m.Attributes.Get(field) != "" {
+			ctx = tmprop.Extract(ctx, textCarrier)
+			return tracer.Start(ctx, options.SpanName)
+		}
+	}
+
+	// if we are set to use only otel
+	// do not fall back to opencensus
+	if options.OnlyOtel {
+		return tracer.Start(ctx, options.SpanName)
+	}
+
+	// fallback to old behaviour (opencensus) if we don't
+	// receive any otel headers
+	traceContextB64 := m.Attributes.Get(traceContextKey)
+	if traceContextB64 == "" {
+		return tracer.Start(ctx, options.SpanName)
+	}
+
+	traceContext, err := base64.StdEncoding.DecodeString(traceContextB64)
+	if err != nil {
+		return tracer.Start(ctx, options.SpanName)
+	}
+
+	spanContext, ok := propagation.FromBinary(traceContext)
+	if !ok {
+		return tracer.Start(ctx, options.SpanName)
+	}
+
+	traceStateString := m.Attributes.Get(traceStateKey)
+	if traceStateString != "" {
+		ts := tracestateFromString(traceStateString)
+		spanContext.Tracestate = ts
+	}
+
+	// convert the opencensus span context to otel
+	otelSpanContext := ocbridge.OCSpanContextToOTel(spanContext)
+	if !otelSpanContext.IsValid() {
+		return tracer.Start(ctx, options.SpanName)
+	}
+
+	return tracer.Start(trace.ContextWithRemoteSpanContext(ctx, otelSpanContext), options.SpanName)
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module github.com/zerofox-oss/go-msg
 go 1.12
 
 require (
-	github.com/google/go-cmp v0.3.0
+	github.com/google/go-cmp v0.5.9
 	github.com/pierrec/lz4/v4 v4.1.8
-	go.opencensus.io v0.22.0
+	go.opencensus.io v0.24.0
+	go.opentelemetry.io/otel v1.19.0
+	go.opentelemetry.io/otel/bridge/opencensus v0.42.0
+	go.opentelemetry.io/otel/sdk v1.19.0
+	go.opentelemetry.io/otel/trace v1.19.0
 )
